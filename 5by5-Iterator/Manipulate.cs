f3f9e994fc07d50d1e874b78d90142a48f98cab3
cauematheus <cauematheus_23@hotmail.com>
2024-05-31 02:09:12 -0300

    Adicionar arquivos de projeto.

---
+++ 5by5-Iterator/Manipulate.cs
@@ -0,0 +1,29 @@
+﻿using _5by5_Iterator;
+using System;
+using System.Collections.Generic;
+
+public class Manipulate
+{
+    public List<Person> Lista { get; private set; }
+    public Stack<Person> Pilha { get; private set; }
+    public Queue<Person> Fila { get; private set; }
+
+    public Manipulate()
+    {
+        Lista = new List<Person>();
+        Pilha = new Stack<Person>();
+        Fila = new Queue<Person>();
+    }
+    public void AddPerson(Person p)
+    {
+        Lista.Add(p);
+        Pilha.Push(p);
+        Fila.Enqueue(p);
+    }
+
+    //Retornar um iterador generico para qualquer manipulação de person
+    public IEnumerator<Person> GetIterator(IEnumerable<Person> collection)
+    {
+        return collection.GetEnumerator();
+    }
+}
---
+++ 5by5-Iterator/Program.cs
@@ -0,0 +1,57 @@
+﻿using _5by5_Iterator;
+using System;
+
+var manipulator = new Manipulate();
+
+
+    manipulator.AddPerson(new Person("Pestana", new Random().Next(1,100)));
+    manipulator.AddPerson(new Person("Fabio", new Random().Next(1, 100)));
+    manipulator.AddPerson(new Person("Andre", new Random().Next(1, 100)));
+
+
+
+Console.WriteLine("FOREACH LISTA");
+foreach (var i in manipulator.Lista)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine("========================");
+Console.WriteLine("FOREACH PILHA");
+foreach (var i in manipulator.Pilha)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine("========================");
+Console.WriteLine("FOREACH FILA");
+foreach (var i in manipulator.Fila)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine("========================");
+
+Console.WriteLine("ITERANDO LISTA");
+var listaIterator = manipulator.GetIterator(manipulator.Lista);
+while (listaIterator.MoveNext())
+    {
+        Console.WriteLine(listaIterator.Current);
+    }
+Console.WriteLine("========================");
+
+Console.WriteLine("ITERANDO PILHA");
+var pilhaIterator = manipulator.GetIterator(manipulator.Pilha);
+while (pilhaIterator.MoveNext())
+{
+    Console.WriteLine(pilhaIterator.Current);
+}
+Console.WriteLine("========================");
+
+Console.WriteLine("ITERANDO FILA");
+var filaIterator = manipulator.GetIterator(manipulator.Fila);
+while (filaIterator.MoveNext())
+{
+
+    Console.WriteLine(filaIterator.Current);
+}
+Console.WriteLine("========================");
+
+
